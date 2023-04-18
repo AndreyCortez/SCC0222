3ef5288026cc17c4be4b46243843105c2627a370
Andrey <andrey.cortez12@gmail.com>
2023-04-18 15:31:01 -0300

    Pratica 3

--- Pratica2/EquacaoDoSegundoGrau.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  long long int a, b, c, x;
-
-  scanf("%lld %lld %lld %lld", &a, &b, &c, &x);
-  printf("%lld\n", a * x * x + b * x + c);
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# SSC0222
-
-Códigos feitos para a matéria de laboratório de introdução à ciência da computação
